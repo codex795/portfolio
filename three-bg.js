5ccf557057f272d1cfa76fb825d6a40064ad5857
codex795 <clashclass012@gmail.com>
2026-09-07 20:58:23 +0530

    update skill

--- three-bg.js
+++ three-bg.js
@@ -10,7 +10,7 @@ const camera = new THREE.PerspectiveCamera(
 window.innerWidth/window.innerHeight,
 0.1,
 1000
-);
+); 
 
 const renderer = new THREE.WebGLRenderer({
 canvas:document.createElement("canvas"),
